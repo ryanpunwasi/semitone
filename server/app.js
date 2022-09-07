176466a65916849305505b0bfb250c197161b020
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-07 18:12:00 -0400

    chore: remove out-of-the-box routes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var notesRouter = require("./routes/notes");
 
 var app = express();
@@ -12,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/api/v1/", notesRouter);
 
 module.exports = app;
