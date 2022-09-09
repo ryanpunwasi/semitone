6a3b3441291397f1a6581a3e1c83597a11792637
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-08 22:31:31 -0400

    feat: add basic route for octaves endpoint

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
-var express = require("express");
-var path = require("path");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
-var notesRouter = require("./routes/notes");
+const octavesRouter = require("./routes/octaves");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/v1/", notesRouter);
+app.use("/api/v1/octaves", octavesRouter);
 
 module.exports = app;
