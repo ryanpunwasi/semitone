90481b1837e70dc4c7d810ca8243b1e325b6d5e1
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-13 13:04:52 -0400

    refactor: use array searching to determine valid octave

--- src/utils/isValidOctave.js
+++ src/utils/isValidOctave.js
@@ -1,11 +1,9 @@
-export const isValidOctave = (octave) => {
+export const isValidOctave = octave => {
   /**
-   * Returns true is octave is equal to '1', 2', '3', '4', or '5'. 
+   * Returns true is octave is equal to '1', 2', '3', '4', or '5'.
    * If otherwise, return false.
    */
-
-  if(octave === '1' || octave === '2' || octave === '3' || octave === '4' || octave === '5') {
-    return true;
-  }
-  return false;
-}
+  const valid = [1, 2, 3, 4, 5];
+  const parsed = parseInt(octave);
+  return valid.includes(parsed);
+};
