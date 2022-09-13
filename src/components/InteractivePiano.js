f95a8220e4a2db30d02315826915aef45767a629
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-13 14:25:04 -0400

    refactor: use functional component

--- src/components/InteractivePiano.js
+++ src/components/InteractivePiano.js
@@ -1,78 +1,79 @@
-import React from 'react';
-import anime from 'animejs';
-import { connect } from 'react-redux';
+import React from "react";
+import anime from "animejs";
+import { connect } from "react-redux";
 
-import { playInteractivePianoNote } from '../actions'
-import './InteractivePiano.css';
-import Image from '../assets/img/keyboard-cropped.svg';
-import LTheme from '../scales/ltheme';
-import FurElise from '../scales/furelise';
-import AngelBeats from '../scales/angelbeats';
-import Mario from '../scales/mario';
- 
+import { playInteractivePianoNote } from "../actions";
+import "./InteractivePiano.css";
+import Image from "../assets/img/keyboard-cropped.svg";
+import LTheme from "../scales/ltheme";
+import FurElise from "../scales/furelise";
+import AngelBeats from "../scales/angelbeats";
+import Mario from "../scales/mario";
 
-class HomeAnimation extends React.Component {
-
-  pianoAnimate = () => {
-    if(this.props.scale === "L's Theme") {
-      let audio = new Audio(LTheme[this.props.currentNote]);
+const InteractivePiano = props => {
+  const animate = () => {
+    if (props.scale === "L's Theme") {
+      let audio = new Audio(LTheme[props.currentNote]);
       audio.load();
       audio.play();
-    } else if(this.props.scale === 'Fur Elise') {
-      let audio = new Audio(FurElise[this.props.currentNote]);
+    } else if (props.scale === "Fur Elise") {
+      let audio = new Audio(FurElise[props.currentNote]);
       audio.load();
       audio.play();
-    } else if(this.props.scale === 'Angel Beats') {
-      let audio = new Audio(AngelBeats[this.props.currentNote]);
+    } else if (props.scale === "Angel Beats") {
+      let audio = new Audio(AngelBeats[props.currentNote]);
       audio.load();
       audio.play();
-    } else if(this.props.scale === 'Mario') {
-      let audio = new Audio(Mario[this.props.currentNote]);
+    } else if (props.scale === "Mario") {
+      let audio = new Audio(Mario[props.currentNote]);
       audio.load();
       audio.play();
     }
-    
-    
-    //let audio = document.getElementById(this.props.currentNote);
-    
-    this.props.playInteractivePianoNote(); 
-    anime({
-      targets: '#interactivePiano',
-      keyframes: [
-        {translateX: 5},
-        {translateX: -5},
-        {translateX: 5},
-        {translateX: 0}
-      ],
-      duration: 400,
-      easing: 'linear',
-      delay: 0
-    });
-};
 
-  render() {
-    return (
-      <section id='animation'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-6 offset-md-3 mx-auto'>
-              <div id='piano' onClick={this.pianoAnimate}>
-                <img id='interactivePiano' className='d-inline-block mx-auto' src={Image} alt='' title={this.props.scale}/>
-                <audio id='audio' src=''></audio>
-              </div>
-            </div> 
+    props.playInteractivePianoNote();
+    anime({
+      targets: "#interactivePiano",
+      keyframes: [
+        { translateX: 2 },
+        { translateX: -2 },
+        { translateX: 2 },
+        { translateX: 0 },
+      ],
+      duration: 200,
+      easing: "linear",
+      delay: 0,
+    });
+  };
+
+  return (
+    <section id="animation">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 offset-md-3 mx-auto">
+            <div id="piano" onClick={animate}>
+              <img
+                id="interactivePiano"
+                className="d-inline-block mx-auto"
+                src={Image}
+                alt="piano"
+                title={props.scale}
+              />
+              <audio id="audio" src=""></audio>
+            </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
-const mapStateToProps = (state) => {
-  return { 
+const mapStateToProps = state => {
+  return {
     currentNote: state.interactivepiano.currentNote,
-    scale: state.interactivepiano.scale
+    scale: state.interactivepiano.scale,
   };
-}
+};
 
-export default connect(mapStateToProps, { playInteractivePianoNote })(HomeAnimation);
+export default connect(mapStateToProps, { playInteractivePianoNote })(
+  InteractivePiano
+);
