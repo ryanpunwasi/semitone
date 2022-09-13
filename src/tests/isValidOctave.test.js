6cfb07f0c89c1389c45d062714090fed3e7e2ca6
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-13 13:03:00 -0400

    test: add tests cases

--- src/tests/isValidOctave.test.js
+++ src/tests/isValidOctave.test.js
@@ -1,9 +1,33 @@
 import { isValidOctave } from "../utils/isValidOctave";
 
 describe("isValidOctave", () => {
-  it("returns true is given the string value of the integers 1 to 5", () => {
+  it("returns true if given the string value 1", () => {
     const actual = isValidOctave("1");
     const expected = true;
+    expect(expected).toBe(actual);
+  });
+
+  it("returns true if given the string value of 5", () => {
+    const actual = isValidOctave("5");
+    const expected = true;
+    expect(expected).toBe(actual);
+  });
+
+  it("returns false if given the string value of 0", () => {
+    const actual = isValidOctave("0");
+    const expected = false;
+    expect(expected).toBe(actual);
+  });
+
+  it("returns false if given the string value of 6", () => {
+    const actual = isValidOctave("6");
+    const expected = false;
+    expect(expected).toBe(actual);
+  });
+
+  it("returns false if given the string value of a", () => {
+    const actual = isValidOctave("a");
+    const expected = false;
     expect(expected).toBe(actual);
   });
 });
