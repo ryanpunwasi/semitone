826930c68b1e58c07255c1c6b82bb654c3c076a1
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-13 12:30:13 -0400

    serve url of static sound files

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 router.get("/:octave/:note", function (req, res, next) {
   const octave = parseInt(req.params.octave);
-  const note = req.params.note;
-  return res.json({ octave, note, src: null });
+  const note = req.params.note.toUpperCase();
+  const src = `localhost:3000/assets/octave-${octave}/${note}.mp3`;
+  return res.json({ octave, note, src });
 });
 
 module.exports = router;
