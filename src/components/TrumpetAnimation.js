4af4fe240ff4b28994d9b0a9c9e2153aa5860efa
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-09-13 15:25:04 -0400

    style: add rotation to animation

--- src/components/TrumpetAnimation.js
+++ src/components/TrumpetAnimation.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import anime from 'animejs';
+import React from "react";
+import anime from "animejs";
 
-import Image from '../assets/img/051-trumpet-cropped.svg';
+import Image from "../assets/img/051-trumpet-cropped.svg";
 
 class TrumpetAnimation extends React.Component {
-  
   componentDidMount() {
     anime({
-      targets: '#trumpetAnimation',
+      targets: "#trumpetAnimation",
       keyframes: [
-        {value: 100, scale: 1.1},
-        {value: 105, translateX: -5},
-        {value: 110, translateX: 5},
-        {value: 115, translateX: 0},
-        {value: 120, translateX: -5},
-        {value: 125, translateX: 5},
-        {value: 130, translateX: 0},
-        {value: 135, translateX: 0},
-        {value: 235, scale: 1}
+        { value: 100, scale: 1.1 },
+        { value: 105, rotate: -10 },
+        { value: 110, translateX: 5 },
+        { value: 115, translateX: 0 },
+        { value: 120, translateX: -5 },
+        { value: 125, translateX: 5 },
+        { value: 130, translateX: 0 },
+        { value: 135, translateX: -5 },
+        { value: 140, translateX: 0 },
+        { value: 220, rotate: 0.5 },
+        { value: 235, scale: 1 },
       ],
-      duration: 800,
-      easing: 'linear',
-      delay: 1500,
-      loop: true
+      duration: 700,
+      easing: "linear",
+      delay: 2000,
+      loop: true,
     });
   }
 
   render() {
     return (
-      <section id='animation'>
-          <div className='container'>
-            <div className='row'>
-              <div className=' col-sm-4 offset-sm-4 col-md-4 offset-md-4 mb-4 mt-2 mx-auto'>
-                <div>
-                  <img id='trumpetAnimation' className='d-inline-block mx-auto mt-0' src={Image} alt=''/>
-                </div>
-              </div> 
+      <section id="animation">
+        <div className="container">
+          <div className="row">
+            <div className=" col-sm-4 offset-sm-4 col-md-4 offset-md-4 mb-4 mt-2 mx-auto">
+              <div>
+                <img
+                  id="trumpetAnimation"
+                  className="d-inline-block mx-auto mt-0"
+                  src={Image}
+                  alt=""
+                />
+              </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     );
   }
 }
