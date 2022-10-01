8a3cd56d05b9dfb3000605211b6913a4451b0908
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-10-01 01:40:06 -0400

    feat: seed notes for first octave

---
+++ server/db/seeds/02_notes.sql
@@ -0,0 +1,21 @@
+--OCTAVE 1--
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'C', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/C_glryf8.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'C', 'sharp', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/Csharp_yb0y3l.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'D', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/D_gl5bfm.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'E', 'flat', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/Dsharp_cls9fb.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'E', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/E_tw7dhf.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'F', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/F_m2kb3y.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'F', 'sharp', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/Fsharp_vdnrui.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'G', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/G_zch156.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'A', 'flat', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/Gsharp_vogszr.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'A', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/A_aoszjq.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'B', 'flat', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/Asharp_ea4usi.mp3');
+INSERT INTO notes (octave_id, letter, accidental, src) VALUES (1, 'B', 'natural', 'https://res.cloudinary.com/dv0dqorkh/video/upload/v1664597381/semitone/octave-1/B_tksr3x.mp3');
+
+--OCTAVE 2--
+
+--OCTAVE 3--
+
+--OCTAVE 4--
+
+--OCTAVE 5--
