d6bfc977c847e0a7a357c67e9a6ee02174eae41b
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-12-09 22:17:31 -0500

    refactor: remove unused function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createPracticeSession } from '../actions';
-import './Button.css';
+import { connect } from "react-redux";
+import { createPracticeSession } from "../actions";
+import "./Button.css";
 
-const Button = (props) => {
-
-  const renderGoogle = () => {
-    if(props.google) {
-      return <i className="bi bi-google me-2"></i>;
-    }
-  }
-
+const Button = props => {
   const submitForm = () => {
     props.createPracticeSession(props.modality, props.submit);
-  }
+  };
 
-  if(props.submit) {
-    if(props.disabled) {
+  if (props.submit) {
+    if (props.disabled) {
       return (
         <div>
-        <button onClick={submitForm} className={`disabled mb-3 button ${props.wide ? 'wide': ''} ${props.color}`} disabled>
-          {props.text}
-        </button>
-      </div>
+          <button
+            onClick={submitForm}
+            className={`disabled mb-3 button ${props.wide ? "wide" : ""} ${
+              props.color
+            }`}
+            disabled
+          >
+            {props.text}
+          </button>
+        </div>
       );
     }
     return (
-      
       <div>
-        <button onClick={submitForm} className={`mb-3 button ${props.wide ? 'wide': ''} ${props.color}`}>
+        <button
+          onClick={submitForm}
+          className={`mb-3 button ${props.wide ? "wide" : ""} ${props.color}`}
+        >
           {props.text}
         </button>
       </div>
@@ -36,24 +36,25 @@ const Button = (props) => {
   } else {
     return (
       <div>
-        <button className={`mb-3 button ${props.wide ? 'wide': ''} ${props.color}`} onClick={props.onClick}>
-          {renderGoogle()}
+        <button
+          className={`mb-3 button ${props.wide ? "wide" : ""} ${props.color}`}
+          onClick={props.onClick}
+        >
           {props.text}
         </button>
       </div>
     );
   }
-  
-}
+};
 
-const mapStateToProps = (state) => {
-  return { 
+const mapStateToProps = state => {
+  return {
     id: state.practice.id,
     currentQuestion: state.practice.currentQuestion,
     mode: state.practice.mode,
     correct: state.practice.correct,
-    questions: state.practice.questions
+    questions: state.practice.questions,
   };
-}
+};
 
 export default connect(mapStateToProps, { createPracticeSession })(Button);
